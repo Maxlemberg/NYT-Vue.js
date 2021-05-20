16147d251c6264ab50e906369a8194e46c215dee
Maksym <74206337+Maxlemberg@users.noreply.github.com>
2021-05-20 14:08:22 +0300

    withoutToken

--- src/services/newsPaper.service.js
+++ src/services/newsPaper.service.js
@@ -1,8 +1,6 @@
 import axiosInst from "../utils/axios";
 
 export const getPopularNews = async () => {
-  const res = await axiosInst.get(
-    "/emailed/1.json?api-key=qEWrF4j6524sPNQ6iqGmfJqzGrxSJnwa"
-  );
+  const res = await axiosInst.get("/emailed/1.json?api-key=");
   return res.data.results;
 };
